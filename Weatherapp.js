9eca45e7da4d858f218aaacc5ed13c2b6c2052e5
Paris Ward <wardparis.pw@gmail.com>
2018-02-25 23:59:47 -0800

    Initial Commit

---
+++ Weatherapp.js
@@ -0,0 +1,59 @@
+//Store global variables
+var url = "https://fcc-weather-api.glitch.me/api/current?";
+var lat, lon;
+var temperatureUnit ='C';
+var temperatureInCelsius;
+var temperatureInFahrenheit;
+
+//Get Current Position (latitude/longitude)
+$(document).ready(function(){
+if(navigator.geolocation) { 
+  navigator.geolocation.getCurrentPosition(function(position){ 
+     lat = "lat=" + position.coords.latitude;
+     lon = "lon=" + position.coords.longitude;
+     getWeather(lat,lon);                                                      
+    });
+  } else{
+      console.log("This Browser does not support geolocation");
+  }
+}) 
+
+//Concatenate weather api with latitude and longitude
+function getWeather(lat,lon) {
+  var apiString = url + lat + "&" + lon;
+  console.log(apiString)
+  //Make ajax call and store response in variables
+    $.ajax({
+      url: apiString, success: function(response) {  
+        var city= response.name;
+        var temperature = Math.round(response.main.temp);
+        var description = response.weather[0].description;
+        var icon = response.weather[0].icon;
+        //Manipulation of HTML elements with JQuery
+        $("#city").html(city);
+        $("#temperature").html(temperature + "°");
+        $("#description").html(description);
+        $("#icon").attr('src',icon);
+        temperatureInCelsius = temperature;
+        temperatureInFahrenheit = Math.round(temperature * 9 / 5 + 32);
+        changeTemperatureUnit(temperatureInCelsius,temperatureInFahrenheit);
+    }
+  });
+}
+//Convert Celsius to Fahrenheit
+
+function changeTemperatureUnit(temperatureInCelsius,temperatureInFahrenheit) {
+  //var temperatureInFahrenheit = Math.round(temperatureInCelsius * 9 / 5 + 32);
+  var temperatureUnit = document.getElementById("temperatureUnit");
+  if(temperatureUnit.innerHTML === temperatureInCelsius) {
+    temperatureUnit.innerHTML = temperatureInFahrenheit;
+    }   else {
+        temperatureUnit.innerHTML = temperatureInCelsius;
+   }
+ 
+}
+    $("button").click(function(){
+   $(this).text($(this).text() == 'C' ? 'F' : 'C');
+      $("h1").text(temperatureInFahrenheit + "°");
+      
+  });
