198bd07a6587b9c7c4131526a67b52c3a92e3ee7
Paris Ward <wardparis.pw@gmail.com>
2018-08-21 08:58:12 -0700

    code clean up, changed to es6 syntax, updated cdns, changed entry point

--- index.js
+++ index.js
@@ -1,49 +1,48 @@
 //Store global variables
-var url = "https://fcc-weather-api.glitch.me/api/current?";
-var lat, lon;
-var temperatureUnit ='C';
-var temperatureInCelsius;
-var temperatureInFahrenheit;
+const url = "https://fcc-weather-api.glitch.me/api/current?";
+let lat, lon;
+let temperatureInCelsius;
+let temperatureInFahrenheit;
 
 //Get Current Position (latitude/longitude)
-$(document).ready(function(){
+$(document).ready(() => {
 if(navigator.geolocation) { 
-  navigator.geolocation.getCurrentPosition(function(position){ 
+  navigator.geolocation.getCurrentPosition((position) => { 
      lat = "lat=" + position.coords.latitude;
      lon = "lon=" + position.coords.longitude;
      getWeather(lat,lon);                                                      
     });
   } else{
-      console.log("This Browser does not support geolocation");
+      error("This Browser does not support geolocation");
   }
 }) 
 
 //Concatenate weather api with latitude and longitude
 function getWeather(lat,lon) {
-  var apiString = url + lat + "&" + lon;
-  console.log(apiString)
+  let apiString = url + lat + "&" + lon;
   //Make ajax call and store response in variables
     $.ajax({
-      url: apiString, success: function(response) {  
-        var city= response.name;
-        var temperature = Math.round(response.main.temp);
-        var description = response.weather[0].description;
-        var icon = response.weather[0].icon;
+      url: apiString, success: (response) => {  
+        let city= response.name;
+        let temperature = Math.round(response.main.temp);
+        let description = response.weather[0].description;
+        let icon = response.weather[0].icon;
         //Manipulation of HTML elements with JQuery
         $("#city").html(city);
         $("#temperature").html(temperature + "°");
         $("#description").html(description);
         $("#icon").attr('src',icon);
        temperatureInCelsius = temperature;
-       temperatureInFahrenheit = Math.round(temperature * 9 / 5 + 32);        changeTemperatureUnit(temperatureInCelsius,temperatureInFahrenheit);
+       temperatureInFahrenheit = Math.round(temperature * 9 / 5 + 32);        
+       changeTemperatureUnit(temperatureInCelsius,temperatureInFahrenheit);
     }
   });
 }
 //Convert Celsius to Fahrenheit
 function changeTemperatureUnit(temperatureInCelsius,temperatureInFahrenheit){
-$("#temperatureUnit").click(function(){
-  var currentTemperatureUnit = $("#temperatureUnit").html();
-  var changedTemperatureUnit = currentTemperatureUnit == 'C' ? 'F' : 'C';
+$("#temperatureUnit").click(() => {
+  let currentTemperatureUnit = $("#temperatureUnit").html();
+  let changedTemperatureUnit = currentTemperatureUnit == 'C' ? 'F' : 'C';
     $("#temperatureUnit").html(changedTemperatureUnit);
       if(
         changedTemperatureUnit == 'F')
